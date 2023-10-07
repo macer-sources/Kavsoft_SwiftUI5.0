a94fc3090c61553714c44d1479f2eb529c44026e
macer-sources <71051274+macer-sources@users.noreply.github.com>
2023-10-07 15:34:06 +0800

    feat: 20. 完成了动画操作，✅。 此demo 可以反复研究

--- A_20_NavigationStack Hero Animation Effect/A_20_NavigationStack Hero Animation Effect/ContentView.swift
+++ A_20_NavigationStack Hero Animation Effect/A_20_NavigationStack Hero Animation Effect/ContentView.swift
@@ -10,16 +10,26 @@ import SwiftUI
 struct ContentView: View {
     @State private var selectedProfile: Profile?
     @State private var pushView: Bool = false
+    @State private var hideView:(Bool, Bool) = (false, false) 
     var body: some View {
         NavigationStack {
             Home(selectedProfile: $selectedProfile, pushView: $pushView)
                 .navigationTitle("Profile")
                 .navigationDestination(isPresented: $pushView) {
-                    if let selectedProfile {
-                        DetailView(profile: selectedProfile)
-                    }
+                    DetailView(selectedProfile: $selectedProfile, pushView: $pushView, hideView: $hideView)
                 }
         }
+        .overlayPreferenceValue(MAnchorKey.self, { value in
+            GeometryReader(content: { geometry in
+                if let selectedProfile, let anchor = value[selectedProfile.id], !hideView.0 {
+                    let rect = geometry[anchor]
+                    ImageView(profile: selectedProfile, size: rect.size)
+                        .offset(x: rect.minX, y: rect.minY)
+                    // simply animating the rect will add the geometry effect we needing
+                        .animation(.snappy(duration: 0.35, extraBounce: 0), value: rect)
+                }
+            })
+        })
     }
 }
 
--- A_20_NavigationStack Hero Animation Effect/A_20_NavigationStack Hero Animation Effect/view/Home.swift
+++ A_20_NavigationStack Hero Animation Effect/A_20_NavigationStack Hero Animation Effect/view/Home.swift
@@ -46,10 +46,12 @@ struct Home: View {
             GeometryReader(content: { geometry in
                 ForEach(profiles) { profile in
                     // fetching each profile image view using the profile id
-                    if let anchor = value[profile.id] {
+                    // hiding the currently tapped view
+                    if let anchor = value[profile.id], selectedProfile?.id != profile.id {
                         let rect = geometry[anchor]
                         ImageView(profile: profile, size: rect.size)
                             .offset(x: rect.minX, y: rect.minY)
+                            .allowsHitTesting(false) // TODO: 禁用此view 响应事件
                     }
                 }
             })
@@ -58,17 +60,65 @@ struct Home: View {
 }
 
 struct DetailView: View {
-    var profile: Profile
+    @Binding var selectedProfile: Profile?
+    @Binding var pushView: Bool
+    @Binding var hideView:(Bool, Bool)
     var body: some View {
-        VStack {
-            GeometryReader(content: { geometry in
-                let size = geometry.size
-                ImageView(profile: profile, size: size)
-            })
-            .frame(height: 400)
-            .ignoresSafeArea()
-            
-            Spacer(minLength: 0)
+        if let selectedProfile {
+            VStack {
+                GeometryReader(content: { geometry in
+                    let size = geometry.size
+                    // Destination View anchor
+                    VStack {
+                        if hideView.0 {
+                            ImageView(profile: selectedProfile, size: size)
+                                .overlay(alignment: .top) {
+                                    Button(action: {
+                                        hideView.0 = false
+                                        hideView.1 = false
+                                        pushView = false
+                                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                                            self.selectedProfile = nil
+                                        }
+                                    }, label: {
+                                        Image(systemName: "xmark")
+                                            .foregroundStyle(.white)
+                                            .padding(10)
+                                            .background(.black, in: .circle)
+                                            .contentShape(.circle)
+                                    })
+                                    .padding(15)
+                                    .padding(.top, 30)
+                                    .frame(maxWidth: .infinity, alignment: .trailing)
+                                    .opacity(hideView.1 ? 1 : 0)
+                                    .animation(.snappy, value: hideView.1)
+                                }
+                                .onAppear {
+                                    DispatchQueue.main.asyncAfter(deadline: .now() ) {
+                                        hideView.1 = true
+                                    }
+                                }
+                        }else {
+                            Color.clear
+                        }
+                    }
+                    .anchorPreference(key: MAnchorKey.self, value: .bounds, transform: { anchor in
+                        return [selectedProfile.id: anchor]
+                    })
+                    
+                })
+                .frame(height: 400)
+                .ignoresSafeArea()
+                
+                Spacer(minLength: 0)
+            }
+            .toolbar(hideView.0 ? .hidden : .visible, for: .navigationBar)
+            .onAppear {
+                // removing the animated view once the animation if finished
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    hideView.0 = true
+                }
+            }
         }
     }
 }
@@ -83,7 +133,18 @@ struct ImageView: View {
             .resizable()
             .aspectRatio(contentMode: .fill)
             .frame(width: size.width, height: size.height)
-            .clipShape(.circle)
+            .overlay(content: {
+                LinearGradient(colors: [
+                    .clear,
+                    .clear,
+                    .clear,
+                    .white.opacity(0.1),
+                    .white.opacity(0.5),
+                    .white.opacity(0.9)
+                ], startPoint: .top, endPoint: .bottom)
+                .opacity(size.width > 60 ? 1 : 0)
+            })
+            .clipShape(.rect(cornerRadius: size.width > 60 ? 0 : 30))
     }
 }
 
