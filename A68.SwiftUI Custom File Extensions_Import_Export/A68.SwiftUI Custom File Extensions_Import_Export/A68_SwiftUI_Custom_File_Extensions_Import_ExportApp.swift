df0b015dea4b633a46e33bb827ff8bba1c1d7d2e
toakan <playtomandjerry>
2024-10-06 11:38:08 +0800

    feat: ✅ over
    https://www.youtube.com/watch?v=Fg1VQsF1RQw&list=PLimqJDzPI-H97JcePxWNwBXJoGS-Ro3a-&index=68&t=44s

---
+++ A68.SwiftUI Custom File Extensions_Import_Export/A68.SwiftUI Custom File Extensions_Import_Export/A68_SwiftUI_Custom_File_Extensions_Import_ExportApp.swift
@@ -0,0 +1,17 @@
+//
+//  A68_SwiftUI_Custom_File_Extensions_Import_ExportApp.swift
+//  A68.SwiftUI Custom File Extensions_Import_Export
+//
+//  Created by Kan Tao on 2024/10/6.
+//
+
+import SwiftUI
+
+@main
+struct A68_SwiftUI_Custom_File_Extensions_Import_ExportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
