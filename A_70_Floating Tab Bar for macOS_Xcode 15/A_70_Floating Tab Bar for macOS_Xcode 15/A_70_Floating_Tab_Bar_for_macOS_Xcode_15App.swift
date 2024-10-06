73297b6f97763dd7590235a733ddb7954e8f2f53
toakan <playtomandjerry>
2024-10-06 12:04:03 +0800

    A_70: init project

---
+++ A_70_Floating Tab Bar for macOS_Xcode 15/A_70_Floating Tab Bar for macOS_Xcode 15/A_70_Floating_Tab_Bar_for_macOS_Xcode_15App.swift
@@ -0,0 +1,17 @@
+//
+//  A_70_Floating_Tab_Bar_for_macOS_Xcode_15App.swift
+//  A_70_Floating Tab Bar for macOS_Xcode 15
+//
+//  Created by Kan Tao on 2024/10/6.
+//
+
+import SwiftUI
+
+@main
+struct A_70_Floating_Tab_Bar_for_macOS_Xcode_15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A_70_Floating Tab Bar for macOS_Xcode 15/A_70_Floating Tab Bar for macOS_Xcode 15/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A_70_Floating Tab Bar for macOS_Xcode 15
+//
+//  Created by Kan Tao on 2024/10/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
