a586892808d922f9d0f0077a40e00e686677b489
toakan <playtomandjerry>
2024-10-06 11:49:30 +0800

    A_99: init project

---
+++ A_99_SwiftUI Range Slider_Xcode 16/A_99_SwiftUI Range Slider_Xcode 16/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A_99_SwiftUI Range Slider_Xcode 16
+//
+//  Created by Kan Tao on 2024/10/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
