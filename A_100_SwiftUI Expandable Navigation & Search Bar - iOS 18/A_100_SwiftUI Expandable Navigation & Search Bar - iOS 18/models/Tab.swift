b26228c4b3647d47d681c19aa48ee20d7943d037
toakan <playtomandjerry>
2024-10-06 11:46:25 +0800

    A_100  feat: final project over ✅
    https://www.youtube.com/watch?v=KiMVgmqtOqY&list=PLimqJDzPI-H97JcePxWNwBXJoGS-Ro3a-&index=100&t=1s

---
+++ A_100_SwiftUI Expandable Navigation & Search Bar - iOS 18/A_100_SwiftUI Expandable Navigation & Search Bar - iOS 18/models/Tab.swift
@@ -0,0 +1,15 @@
+//
+//  Tab.swift
+//  A_100_SwiftUI Expandable Navigation & Search Bar - iOS 18
+//
+//  Created by Kan Tao on 2024/10/6.
+//
+
+import SwiftUI
+
+enum Tab: String, CaseIterable {
+    case all = "All"
+    case personal = "Personal"
+    case office = "Updates"
+    case community = "Gaming"
+}
