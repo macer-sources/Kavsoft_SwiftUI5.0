84ed3e59e4dfcbbd5fbb2fba6757670eb035a549
toakan <playtomandjerry>
2024-10-06 11:45:24 +0800

    feat: init project

---
+++ A_100_SwiftUI Expandable Navigation & Search Bar - iOS 18/A_100_SwiftUI Expandable Navigation & Search Bar - iOS 18/A_100_SwiftUI_Expandable_Navigation___Search_Bar___iOS_18App.swift
@@ -0,0 +1,17 @@
+//
+//  A_100_SwiftUI_Expandable_Navigation___Search_Bar___iOS_18App.swift
+//  A_100_SwiftUI Expandable Navigation & Search Bar - iOS 18
+//
+//  Created by Kan Tao on 2024/10/6.
+//
+
+import SwiftUI
+
+@main
+struct A_100_SwiftUI_Expandable_Navigation___Search_Bar___iOS_18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
