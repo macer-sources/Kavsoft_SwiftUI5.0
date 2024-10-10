d3e7cdf69a473c55ca13bd6c0aaf21a0dd9f22fc
toakan <playtomandjerry>
2024-10-10 21:36:23 +0800

    A_71: init project

---
+++ A_53_Infinite Looping ScrollView/A_71_SwiftUI Dock Progress Bar for macOS Xcode 15/A_71_SwiftUI Dock Progress Bar for macOS Xcode 15/A_71_SwiftUI_Dock_Progress_Bar_for_macOS_Xcode_15App.swift
@@ -0,0 +1,17 @@
+//
+//  A_71_SwiftUI_Dock_Progress_Bar_for_macOS_Xcode_15App.swift
+//  A_71_SwiftUI Dock Progress Bar for macOS Xcode 15
+//
+//  Created by Kan Tao on 2024/10/10.
+//
+
+import SwiftUI
+
+@main
+struct A_71_SwiftUI_Dock_Progress_Bar_for_macOS_Xcode_15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A_53_Infinite Looping ScrollView/A_71_SwiftUI Dock Progress Bar for macOS Xcode 15/A_71_SwiftUI Dock Progress Bar for macOS Xcode 15/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A_71_SwiftUI Dock Progress Bar for macOS Xcode 15
+//
+//  Created by Kan Tao on 2024/10/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
