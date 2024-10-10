f3f10819a8d022f4add7b21e9f3bce544dd15e65
toakan <playtomandjerry>
2024-10-10 21:39:15 +0800

    A_71: final project ✅
    https://www.youtube.com/watch?v=m7t1GnXFARY&list=PLimqJDzPI-H97JcePxWNwBXJoGS-Ro3a-&index=71&t=1s

--- A_53_Infinite Looping ScrollView/A_71_SwiftUI Dock Progress Bar for macOS Xcode 15/A_71_SwiftUI Dock Progress Bar for macOS Xcode 15/ContentView.swift
+++ A_53_Infinite Looping ScrollView/A_71_SwiftUI Dock Progress Bar for macOS Xcode 15/A_71_SwiftUI Dock Progress Bar for macOS Xcode 15/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var viewModel = DockProgress.shared
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        VStack(spacing: 20, content: {
+            Picker("", selection: $viewModel.type) {
+                ForEach(DockProgress.ProgressType.allCases, id:\.rawValue) {item in
+                    Text(item.rawValue)
+                        .tag(item)
+                }
+            }
+            .pickerStyle(.segmented)
+            
+            Toggle("Show Dock Progress", isOn: $viewModel.isVisiable)
+                .toggleStyle(.switch)
+        })
+        .padding(15)
+        .frame(width: 200, height: 200)
+        .onReceive(Timer.publish(every: 0.1, on: .main, in: .common).autoconnect(), perform: { _ in
+            guard viewModel.isVisiable else {return}
+            if viewModel.progress >= 1.0 {
+                viewModel.isVisiable = false
+                viewModel.progress = .zero
+            } else {
+                viewModel.progress += 0.007
+            }
+        })
     }
 }
 
