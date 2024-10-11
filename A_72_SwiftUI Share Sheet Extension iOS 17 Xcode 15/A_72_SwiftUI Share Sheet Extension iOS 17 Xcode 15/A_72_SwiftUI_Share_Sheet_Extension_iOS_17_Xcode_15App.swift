cf3461409e6c42503d3948387519fe5284a15567
toakan <playtomandjerry>
2024-10-11 23:02:36 +0800

    A_72: init project

---
+++ A_72_SwiftUI Share Sheet Extension iOS 17 Xcode 15/A_72_SwiftUI Share Sheet Extension iOS 17 Xcode 15/A_72_SwiftUI_Share_Sheet_Extension_iOS_17_Xcode_15App.swift
@@ -0,0 +1,17 @@
+//
+//  A_72_SwiftUI_Share_Sheet_Extension_iOS_17_Xcode_15App.swift
+//  A_72_SwiftUI Share Sheet Extension iOS 17 Xcode 15
+//
+//  Created by Kan Tao on 2024/10/11.
+//
+
+import SwiftUI
+
+@main
+struct A_72_SwiftUI_Share_Sheet_Extension_iOS_17_Xcode_15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A_72_SwiftUI Share Sheet Extension iOS 17 Xcode 15/A_72_SwiftUI Share Sheet Extension iOS 17 Xcode 15/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A_72_SwiftUI Share Sheet Extension iOS 17 Xcode 15
+//
+//  Created by Kan Tao on 2024/10/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
