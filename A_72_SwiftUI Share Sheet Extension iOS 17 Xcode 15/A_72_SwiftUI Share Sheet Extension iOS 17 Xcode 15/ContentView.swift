82e140b83ed3dd37292aaf7ba1fb7e7c8670b078
toakan <playtomandjerry>
2024-10-11 23:05:39 +0800

    A_72: final project ✅
    https://www.youtube.com/watch?v=_YJPnTQ8R1A&list=PLimqJDzPI-H97JcePxWNwBXJoGS-Ro3a-&index=72

--- A_72_SwiftUI Share Sheet Extension iOS 17 Xcode 15/A_72_SwiftUI Share Sheet Extension iOS 17 Xcode 15/ContentView.swift
+++ A_72_SwiftUI Share Sheet Extension iOS 17 Xcode 15/A_72_SwiftUI Share Sheet Extension iOS 17 Xcode 15/ContentView.swift
@@ -6,18 +6,58 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Query private var items:[ImageItemData]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView(.vertical) {
+                LazyVStack(spacing: 10, content: {
+                    ForEach(items, id: \.self) { item in
+                        CardView(item: item)
+                            .frame(height: 250)
+                    }
+                })
+                .padding(15)
+            }
         }
-        .padding()
     }
 }
+
+struct CardView: View {
+    var item: ImageItemData
+    @State private var previewImage: UIImage?
+    var body: some View {
+        GeometryReader(content: { geometry in
+            let size = geometry.size
+            if let previewImage = previewImage {
+                Image(uiImage: previewImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: size.width, height: size.height)
+            } else {
+                ProgressView()
+                    .frame(width: size.width, height: size.height)
+                    .task {
+                        generatePreviewImage(size)
+                    }
+            }
+            
+        })
+    }
+    
+    private func generatePreviewImage(_ size: CGSize) {
+        Task.detached(priority: .high) {
+            let thumbnail = UIImage(data: item.data)?.preparingThumbnail(of: size)
+            await MainActor.run {
+                previewImage = thumbnail
+            }
+        }
+    }
+    
+}
+
 
 #Preview {
     ContentView()
